c766206d1e819367a0593ae98502506d48c63040
Calcifer <lordofthemaracas@gmail.com>
2017-04-11 20:07:26 -0400

    first pass at a setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -sf DotFiles/.bashrc .bashrc
+ln -sf DotFiles/.config/bash .config/bash
+ln -sf DotFiles/.gitconfig .gitconfig
+ln -sf DotFiles/.gitignore .gitignore
+ln -sf DotFiles/.hgrc .hgrc
