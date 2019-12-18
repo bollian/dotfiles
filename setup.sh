ead5cbb39f3178e59f7bec444a87568f8133047b
Ian Boll <iangboll@vt.edu>
2019-12-18 18:05:16 -0500

    juiced up the setup script to be a little safer

--- setup.sh
+++ setup.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
-lnr() {
-	mkdir -p `dirname "$2"` || true
-	ln -sf `pwd`/"$1" "$2"
+repo_dir=$(dirname "$0")
+
+# safe link w/ recursive directory creation
+slnr() {
+    target=$repo_dir/$1
+    link=$HOME/$2
+
+    if [ -s "$link" ]; then
+        echo "'$2' already linked"
+    elif [ -e "$link" ]; then
+        echo "Not able to create config file '$2': Already exists." >&2
+    else
+        mkdir -p `dirname "$link"` # create any directories leading to the link file
+        ln -s "$target" "$link"
+    fi
 }
 
-# change to user directory
-cd
-
-lnr dotfiles/.bashrc .bashrc
-lnr dotfiles/.config/bash .config/bash
-lnr dotfiles/.config/git .config/git
-lnr dotfiles/.config/nvim .config/nvim
-lnr dotfiles/.hgrc .hgrc
+slnr dotfiles/.bashrc .bashrc
+slnr dotfiles/.config/bash .config/bash
+slnr dotfiles/.config/git .config/git
+slnr dotfiles/.config/nvim .config/nvim
+slnr dotfiles/.hgrc .hgrc
 
