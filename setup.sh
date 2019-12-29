f12e7af8a14a195aa00e0bf4738c9b5d52be84f9
Ian Boll <iangboll@vt.edu>
2019-12-29 03:09:27 -0500

    - added gebaar-libinput as a systemd user service - added support for multiple link locations in setup script

--- setup.sh
+++ setup.sh
@@ -5,16 +5,22 @@
 # safe link w/ recursive directory creation
 slnr() {
     target=$HOME/dotfiles/$1
-    link=$HOME/$2
+    shift # discard $1 so we can loop through all arguments afterwards
 
-    if [ -h "$link" ]; then
-        echo "'$2' already linked"
-    elif [ -e "$link" ]; then
-        echo "Not able to create config file '$2': Already exists." >&2
-    else
-        mkdir -p `dirname "$link"` # create any directories leading to the link file
-        ln -s "$target" "$link"
-    fi
+    while test $# -gt 0; do
+        link=$HOME/$1
+
+        if [ -h "$link" ]; then
+            echo "'$1' already linked"
+        elif [ -e "$link" ]; then
+            echo "'$1' already exists" >&2
+        else
+            mkdir -p $(dirname "$link") # create any directories leading to the link file
+            ln -s "$target" "$link"
+        fi
+
+        shift # discard this argument
+    done
 }
 
 # cd into home directory
@@ -26,4 +32,10 @@ slnr .config/git  .config/git
 slnr .config/nvim .config/nvim
 slnr .hgrc        .hgrc
 slnr .ssh/config  .ssh/config
+slnr .local/share/systemd/user/gebaard.service \
+     .local/share/systemd/user/gebaard.service
+slnr .config/gebaar/gebaard.toml .config/gebaar/gebaard.toml
 
+systemctl --user daemon-reload
+systemctl --user enable gebaard.service
+systemctl --user start gebaard.service
