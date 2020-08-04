ada5e25112607edca94760f6d3e7da8955d835c6
Ian Boll <iangboll@vt.edu>
2020-08-04 17:48:53 -0400

    moved systemd service to .config, added hud-menu and ksuperkey

--- setup.sh
+++ setup.sh
@@ -32,10 +32,17 @@ slnr .config/git  .config/git
 slnr .config/nvim .config/nvim
 slnr .hgrc        .hgrc
 slnr .ssh/config  .ssh/config
-slnr .local/share/systemd/user/gebaard.service \
-     .local/share/systemd/user/gebaard.service
+slnr .config/systemd/user/gebaard.service \
+     .config/systemd/user/gebaard.service
+slnr .config/systemd/user/ksuperkey.service \
+     .config/systemd/user/ksuperkey.service
+slnr .config/systemd/user/hud-menu.service \
+     .config/systemd/user/hud-menu.service
 slnr .config/gebaar/gebaard.toml .config/gebaar/gebaard.toml
 
+# reload so that systemd picks up any new services
 systemctl --user daemon-reload
-systemctl --user enable gebaard.service
-systemctl --user start gebaard.service
+
+systemctl --user enable --now gebaard.service
+systemctl --user enable --now ksuperkey.service
+systemctl --user enable --now hud-menu.service
