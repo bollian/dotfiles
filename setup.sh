041b67fdc4f6a92a3ec66e33f6dd50d08f31a36c
Ian Boll <iangboll@vt.edu>
2021-01-07 15:27:38 -0500

    added ipfs daemon as a session service. ksuperkey disabled by default

--- setup.sh
+++ setup.sh
@@ -26,6 +26,8 @@ slnr() {
 # cd into home directory
 cd
 
+ipfs init || true
+
 slnr .bashrc      .bashrc
 slnr .config/bash .config/bash
 slnr .config/git  .config/git
@@ -38,6 +40,8 @@ slnr .config/systemd/user/ksuperkey.service \
      .config/systemd/user/ksuperkey.service
 slnr .config/systemd/user/hud-menu.service \
      .config/systemd/user/hud-menu.service
+slnr .config/systemd/user/ipfs.service \
+     .config/systemd/user/ipfs.service
 slnr .config/gebaar/gebaard.toml .config/gebaar/gebaard.toml
 slnr .local/share/nvim/site/autoload/plug.vim \
      .local/share/nvim/site/autoload/plug.vim
@@ -45,6 +49,5 @@ slnr .local/share/nvim/site/autoload/plug.vim \
 # reload so that systemd picks up any new services
 systemctl --user daemon-reload
 
-systemctl --user enable --now gebaard.service
-systemctl --user enable --now ksuperkey.service
 systemctl --user enable --now hud-menu.service
+systemctl --user enable --now ipfs.service
